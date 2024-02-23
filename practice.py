15b8e6128e3371e086a449aaa428da69be9a6e40
Python Developer <161020937+maicstel@users.noreply.github.com>
2024-02-23 02:17:02 -0800

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,20 @@
+# def hello(name):
+#     print("good morning",name)
+# hello("ayo")
+# num1= "tola"
+# import constant
+# print(constant.PI/2)
+# mykids= ("tola","tosin","titi","tayo")
+# def mykids(*kids):
+#      print("out of all my kids the one i cherish the most is",kids[3])
+# mykids=("tola","tosin","titi","tayo")
+class Tobiloba:
+    def __init__(self,fname,lname) :
+        self.fname=fname
+        self.lname=lname
+        fname="ayomipojojo"
+        lname="taxia"
+x=Tobiloba("ayomippojoo","taxa")
+print(x)
+
+        
